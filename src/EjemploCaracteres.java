a84de8271998a7d4a065054b90af02446e5169ff
Luis Cuellar <luisenri2901@gmail.com>
2023-02-18 22:01:21 -0500

    Se exploran las variables char y string

---
+++ src/EjemploCaracteres.java
@@ -0,0 +1,13 @@
+
+public class EjemploCaracteres {
+	public static void main(String[] args) {
+		char caracter = 'a'; //Un solo caracter entre comillas simples
+		System.out.println(caracter);
+		
+		caracter = 65; //Posicion 65 en el codigo ascii
+		System.out.println(caracter);
+		
+		String palabra = "Permite poner más de una palabra";
+		System.out.println(palabra);
+	}
+}
