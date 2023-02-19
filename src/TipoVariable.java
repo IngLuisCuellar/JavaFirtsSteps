cdb8e71c9ec923c461b135da1f25a13ce265e304
Luis Cuellar <luisenri2901@gmail.com>
2023-02-18 19:24:52 -0500

    Se realiza una exploración inicial de los tipos de variables en java

--- src/TipoVariable.java
+++ src/TipoVariable.java
@@ -3,6 +3,23 @@ public class TipoVariable {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!");
+		
+		int edad = 22;
+		System.out.println("Mi edad es " + edad);
+		
+		double salario = 122.587;
+		System.out.println("El salario es " + salario);
+		
+		//Ejemplo de Cast
+		
+		double variable = 125.35;
+		int variableEntero = (int) variable; //Cast
+		System.out.println("La variable " + variable + " en entero es " + variableEntero);
+		
+		long prueba = 12222222222222222L; //Es necesario etiquetar el número con una L, denotando que es una variable tipo long
+		short numeroPequenho = 1320;
+		byte numerMasPequenho = 12;
+		float numeroDecimalPequenho = 2.5F;
 	}
 
 }
