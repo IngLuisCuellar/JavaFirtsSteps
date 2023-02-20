7c54dc2d6bb6b7712a5612cf9edc622f4f2ed82c
Luis Cuellar <luisenri2901@gmail.com>
2023-02-19 22:48:53 -0500

    Se exploran las variables booleanas en condiciones

--- src/EjemploCondicionales.java
+++ src/EjemploCondicionales.java
@@ -4,12 +4,14 @@ public class EjemploCondicionales {
 	public static void main(String[] args) {
 		System.out.println("Condicionales");
 
-		int edad = 17;
+		int edad = 21;
 		int cantidadPersonas = 2;
+		
+		boolean esPareja = true; //Almacena valor de true o false 
 
 		// || --> Condicional O
 		// && --> Condicional y
-		if (edad >= 18 || cantidadPersonas >= 2) {
+		if (edad >= 18 && esPareja) {
 			System.out.println("Usted puede entrar");
 		} else {
 			System.out.println("Usted no está permitido para entrar");
