6c0f99bb0f3a9be029c803942524c7af1525d0d8
Luis Cuellar <luisenri2901@gmail.com>
2023-02-20 20:57:15 -0500

    Haciendo un cuadrado de asteríscos (una matriz)

---
+++ src/EjercicioMatriz.java
@@ -0,0 +1,17 @@
+
+public class EjercicioMatriz {
+	public static void main(String[] args) {
+		System.out.println("Matriz de asteríscos");
+		System.out.println();
+		
+		for(int fila = 0; fila <=10; fila++) {
+			for(int columna = 0 ; columna <= 10; columna++) {
+				System.out.print("*");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
